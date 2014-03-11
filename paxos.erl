106e1e7fb1a5b7c678d456126680c214d89ae355
Kristian Sällberg <kristian@purestyle.se>
2014-03-11 21:00:40 +0100

    further coding

--- paxos.erl
+++ paxos.erl
@@ -1,6 +1,7 @@
 % TODO: now only for one process, implement for x procs
 % TODO: test it
 % TODO: now only phase 1, implement phase 2
+% TODO: clean up some weird things
 -module(paxos).
 -export([start/0]).
 
@@ -9,16 +10,29 @@ start() ->
    % upon event init
    Pid = spawn(paxos,
                loop,
-               [1,0,0,{0,undefined},{0,undefined},[undefined],0]
+               [not_instantiated]
               ).
 
+yes_no(true,ForTrue,_)   -> ForTrue;
+yes_no(false,_,ForFalse) -> ForFalse.
+
+% this is before a paxos process is instantiated, 
+% it will just wait to be kicked off
+loop(not_instantiated) ->
+    receive
+        % initialize values
+       {init,{N,_T_,Prepts_,A_,P_,ReadLs_,Acks_}} ->
+           loop(N,Rank,T_,Prepts_,A_,P_,ReadLs_,Acks_)
+    end;
 % actors/states: proposer, acceptor, learner
-loop(Rank,T,Prepts,A={Ats,Av},P={Pts,Pv},ReadLs,Acks) ->
+% N is all procs
+% readList is of type [Pid,{}]
+loop(N,Rank,T,Prepts,A={Ats,Av},P={Pts,Pv},ReadLs,Acks) ->
    receive
        % phase 1 propose
        {propose,V} ->
            % trigger < beb, Broadcast | [Prepare, pts, t] >
-           loop(Rank,T+1,Prepts,A,{(T+1)*length(ReadLs)*Rank,V},[undefined],0);
+           loop(N,Rank,T+1,Prepts,A,{(T+1)*length(ReadLs)*Rank,V},[undefined],0);
        
        % phase 1 deliver prepare
        {deliver,Q,{prepare,Ts,T_}} ->
@@ -26,10 +40,10 @@ loop(Rank,T,Prepts,A={Ats,Av},P={Pts,Pv},ReadLs,Acks) ->
            case Ts < Prepts of
                false ->
                    % broadcast
-                   loop(Rank,NewT,Ts,A,P,ReadLs,Acks);
+                   loop(N,Rank,NewT,Ts,A,P,ReadLs,Acks);
                true ->
                    % broadcast
-                   loop(Rank,NewT,Prepts,A,P,ReadLs,Acks)
+                   loop(N,Rank,NewT,Prepts,A,P,ReadLs,Acks)
            end;
            
        % phase 2 deliver nack
@@ -37,16 +51,28 @@ loop(Rank,T,Prepts,A={Ats,Av},P={Pts,Pv},ReadLs,Acks) ->
            NewT = max(T,T_)+1,
            case Pts == Pts_ of
                false ->
-                   loop(Rank,NewT,Prepts,A,P,ReadLs,Acks);
+                   loop(N,Rank,NewT,Prepts,A,P,ReadLs,Acks);
                true ->
-                   loop(Rank,NewT,Prepts,A,{0,Pv},ReadLs,Acks) % Pts = 0
+                   loop(N,Rank,NewT,Prepts,A,{0,Pv},ReadLs,Acks) % Pts = 0
                    % trigger abort
            end;
            
        % todo fix
        {deliver,Q,{prepare_ack,Ts,V,Pts_,T_}} ->
            NewT = max(T,T_)+1,
-           undefined;
+           case Pts_ == Pts of
+               true ->
+                   % replace list of Q
+                   case length(ReadLs) > length(N) / 2 of
+                       true ->
+                           (NewTs,NewV) = highest(ReadLs),
+                           NewPv = yes_no(ts/=0,NewV,Pv),
+                           NewReadLs = [],
+                           % todo broadcast accept
+                           loop(N,Rank,NewT,Prepts,{Ats,Av},{Pts,NewV},ReadLs,Acks)
+                   end
+           end,
+           loop(N,Rank,T,Prepts,A,P,ReadLs,Acks);
        
        {deliver,Q,{accept,Ts,V,T_}} ->
            NewT = max(T,T_)+1,
@@ -55,15 +81,26 @@ loop(Rank,T,Prepts,A={Ats,Av},P={Pts,Pv},ReadLs,Acks) ->
                    % ats = prepts = ts
                    % av  = v
                    %trigger
-                   loop(Rank,NewT,Ts,{Ts,V},P,ReadLs,Acks);
+                   loop(N,Rank,NewT,Ts,{Ts,V},P,ReadLs,Acks);
                true ->
                    % trigger
-                   loop(Rank,NewT,Prepts,A,{0,Pv},ReadLs,Acks)
+                   loop(N,Rank,NewT,Prepts,A,{0,Pv},ReadLs,Acks)
            end;
        
        % todo fix
        {deliver,Q,{accept_ack,Pts_,T_} ->
            NewT = max(T,T_)+1,
-           undefined
-           
+           case Pts_ == Pts of
+               true ->
+                   NewAcks = Acks + 1,
+                   
+                   case NewAcks > N / 2 of
+                       true ->  NewPts = 0,
+                                % trigger ac return
+                                loop(N,Rank,NewT,Ts,A,{NewPts,Pv},ReadLs,NewAcks);
+                       false -> loop(N,Rank,NewT,Ts,A,P,ReadLs,NewAcks)
+                   end;
+                   
+               false -> loop(N,Rank,NewT,Ts,A,P,ReadLs,Acks)
+           end
    end.
