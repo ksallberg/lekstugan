9495e63fd05bc2698ee48e84f84a896b1d3a0074
kristian@purestyle.se <kristian@purestyle.se>
2014-07-21 22:27:45 +0200

    more rand

--- Rand.hs
+++ Rand.hs
@@ -4,7 +4,9 @@ main :: IO ()
 main = do putStrLn "testing random"
           randGen <- getStdGen
           let (theResult,newSeed) = theRandomInt randGen
-          putStrLn $ "random value: " ++ show theResult
+          let (theResult2,_)      = theRandomInt newSeed
+          putStrLn $ "random value: "  ++ show theResult
+          putStrLn $ "random value2: " ++ show theResult2
 
 theRandomInt :: StdGen -> (Integer,StdGen)
 theRandomInt gen = randomR (0,100) gen
