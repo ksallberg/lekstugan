35b8b480f3e8d8bd6514e738c77889d99ec8a337
kristian@purestyle.se <kristian@purestyle.se>
2014-11-12 19:02:16 +0100

    encrypt sec

---
+++ enctrypt.hs
@@ -0,0 +1,14 @@
+import Data.Char (ord, toUpper)
+import Data.Bits
+import Data.List
+import Control.Monad
+import Numeric
+
+cong :: Int -> Int
+cong input = (69 * input + 113) `mod` 256
+
+answer :: [Int]
+answer = map (uncurry xor) $ zip (map ord "Safe") (take 4 (iterate cong 43))
+
+hexAnswer :: String
+hexAnswer = map toUpper $ intercalate " " $ map ((flip showHex) "") answer
