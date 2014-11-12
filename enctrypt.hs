a98d2841b20279522a965dbcc66b4ae5d885de44
kristian@purestyle.se <kristian@purestyle.se>
2014-11-12 19:29:24 +0100

    hello

--- enctrypt.hs
+++ enctrypt.hs
@@ -3,11 +3,23 @@ import Data.Bits
 import Data.List
 import Numeric
 
+type PlainText = String
+
 cong :: Int -> Int
-cong input = (69 * input + 113) `mod` 256
+cong char = (69 * char + 113) `mod` 256
 
-answer :: [Int]
-answer = map (uncurry xor) $ zip (map ord "Safe") (take 4 (iterate cong 43))
+answer :: PlainText -> Int -> [Int]
+answer txt start = map (uncurry xor) $ zip plainTextAscii key
+    where plainTextAscii = map ord txt
+          key            = take (length txt) $ iterate cong start
 
-hexAnswer :: String
-hexAnswer = map toUpper $ intercalate " " $ map ((flip showHex) "") answer
+hexAnswer :: [Int] -> String
+hexAnswer ans = let toHex  = (flip showHex) ""
+                    joined = intercalate " " $ map toHex ans
+                in map toUpper joined
+
+q7 :: String
+q7 = hexAnswer $ answer "Safe" 43
+
+q10 :: String
+q10 = undefined
