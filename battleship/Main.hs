e23f2ea93fe6afc90a8672bb156224ce8485761b
kristian@purestyle.se <kristian@purestyle.se>
2015-11-08 15:07:22 +0100

    Localhost in battleship

--- battleship/Main.hs
+++ battleship/Main.hs
@@ -81,7 +81,7 @@ instance ToJSON RequestData where
                                                     "shoot_at"    .= shot]
 
 hostName :: String
-hostName = "http://192.168.1.89:28251"
+hostName = "http://localhost:28251"
 
 main :: IO ()
 main = do
