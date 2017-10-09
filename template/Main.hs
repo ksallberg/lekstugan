29da7156e20e2359872150c8a97f4e0089286a56
Kristian Sällberg <kristian@purestyle.se>
2017-10-09 20:27:36 +0200

    template example from some site

--- template/Main.hs
+++ template/Main.hs
@@ -4,5 +4,8 @@ module Main where
 
 import PrintF (printf)
 
+main :: IO ()
+main = putStrLn test
+
 test :: String
-test = $(printf "Error: %s on line %d" "Bad var" 123
+test = $(printf "Error: %s on line %d") "Bad var" 123
--- template/PrintF.hs
+++ template/PrintF.hs
@@ -1,8 +1,44 @@
 {-# LANGUAGE TemplateHaskell #-}
-
 module PrintF where
 
+-- NB: printf needs to be in a separate module to the one where
+-- you intend to use it.
+
+-- Import some Template Haskell syntax
 import Language.Haskell.TH
 
+-- Possible string tokens: %d %s and literal strings
 data Format = D | S | L String
-  deriving Show
+    deriving Show
+
+-- a poor man's tokenizer
+tokenize :: String -> [Format]
+tokenize [] = []
+tokenize ('%':'d':rest) = D : tokenize rest
+tokenize ('%':'s':rest) = S : tokenize rest
+tokenize (s:str) = L (s:p) : tokenize rest -- so we don't get stuck on weird '%'
+    where (p, rest) = span (/= '%') str
+
+-- generate argument list for the function
+args :: [Format] -> [PatQ]
+args fmt = concatMap (\(f,n) -> case f of
+                                  L _ -> []
+                                  _   -> [varP n]) $ zip fmt names
+    where names = [ mkName $ 'x' : show i | i <- [0..] ]
+
+-- generate body of the function
+body :: [Format] -> ExpQ
+body fmt = foldr (\ e e' -> infixApp e [| (++) |] e') (last exps) (init exps)
+    where exps = [ case f of
+                    L s -> stringE s
+                    D   -> appE [| show |] (varE n)
+                    S   -> varE n
+                 | (f,n) <- zip fmt names ]
+          names = [ mkName $ 'x' : show i | i <- [0..] ]
+
+-- glue the argument list and body together into a lambda
+-- this is what gets spliced into the haskell code at the call
+-- site of "printf"
+printf :: String -> Q Exp
+printf format = lamE (args fmt) (body fmt)
+    where fmt = tokenize format
