ef70a147af55af9991321959169a305efba56a7e
Kristian Sällberg <kristian@purestyle.se>
2017-10-11 21:49:27 +0200

    explanation

--- template/Main.hs
+++ template/Main.hs
@@ -9,5 +9,9 @@ import PrintF (printf)
 main :: IO ()
 main = putStrLn test
 
+-- '$(' betyder splice. alltså att uttrycket
+--   '$(printf "Error: %s on line %d")'
+-- blir ett nytt program compile-time, med rätt typer,
+-- beroende på vilken input som ges efter
 test :: String
 test = $(printf "Error: %s on line %d") "Bad var" 123
