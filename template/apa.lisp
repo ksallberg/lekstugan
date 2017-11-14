bdc8882d1e319a56fae6e266a1dc983d14a86480
kristian sallberg <kristian@purestyle.se>
2017-11-14 19:56:29 +0100

    hello world lisp

---
+++ template/apa.lisp
@@ -0,0 +1,14 @@
+;; sh> clisp
+;; sh> ccl
+
+;; ...
+
+;; Break 2 [3]> (load "apa.lisp")
+;; ;; Loading file apa.lisp ...
+;; ;; Loaded file apa.lisp
+;; T
+;; Break 2 [3]> (hello) <- anropa funktionen hello
+;; "HELLO WORLD"
+
+(DEFUN HELLO ()
+  "HELLO WORLD")
