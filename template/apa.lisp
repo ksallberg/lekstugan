b350e665b583bb7fb618c38506fa919046404191
kristian sallberg <kristian@purestyle.se>
2017-11-15 21:43:51 +0100

    hej2

--- template/apa.lisp
+++ template/apa.lisp
@@ -1,5 +1,7 @@
 ;; https://stackoverflow.com/questions/267862/what-makes-lisp-macros-so-special
 
+;; http://cl-cookbook.sourceforge.net/macros.html
+
 ;; sh> clisp
 ;; sh> ccl
 
@@ -53,6 +55,29 @@
 ;; Break 8 [9]>
 ;; (lcomp x for x in (range 10) if (= (mod x 2) 0))
 ;; (0 2 4 6 8)
+
+;; Backquote
+
+;; Before taking another step, we need to introduce a piece of Lisp notation
+;; that is indispensable to defining macros, even though technically it is
+;; quite independent of macros. This is the backquote facility. As we saw above,
+;; the main job of a macro, when all is said and done, is to define a piece of
+;; Lisp code, and that means evaluating expressions such as
+;; (list 'prog (list 'setq ...) ...). As these expressions grow in complexity,
+;; it becomes hard to read them and write them. What we find ourselves wanting
+;; is a notation that provides the skeleton of an expression, with some of the
+;; pieces filled in with new expressions. That's what backquote provides.
+;; Instead of the the list expression given above, one writes:
+;; `(progn (setq ,v1 ,e) (setg ,v2 ,e))
+
+;; The backquote (`) character signals that in the expression
+;; that follows, every subexpression not preceded by a comma
+;; is to be quoted, and every subexpression preceded by a comma
+;; is to be evaluated.
+
+;; ----> alltsa, inom `:
+;; ----> comma ar for att fylla i ett varde in i koden
+;;          kod in i koden
 
 (defmacro lcomp (expression for var in list conditional conditional-test)
   ;; create a unique variable name for the result
