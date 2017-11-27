c31927724b56270060a4c6dd5c36dec785af4c58
kristian sallberg <kristian@purestyle.se>
2017-11-27 21:06:33 +0100

    hej

--- lisp/test.lisp
+++ lisp/test.lisp
@@ -1,3 +1,13 @@
 (+ 10 29)
 
 (format t "Hello world!~%")
+
+(defun get-file (filename)
+  (with-open-file (stream filename)
+                  (loop for line = (read-line stream nil)
+                        while line
+                        collect line)))
+
+(setq x 10)
+(format t "x: ~D ~%"
+        x)
