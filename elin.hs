e535cf8fd5dff61ad25d06f5acbed302b6866e91
Kristian Sallberg <krisallb@cisco.com>
2018-09-12 19:09:14 +0200

    radiohead

--- elin.hs
+++ elin.hs
@@ -1,10 +1,16 @@
 module Elin where
 
 import Control.Monad (forM_)
-import Prelude hiding((*))
+import Prelude hiding((*), (+))
+
+import qualified Prelude as P
 
 (*) :: (Int -> IO()) -> Int -> IO ()
 (*) f x = forM_ [1..x] f
+
+(+) :: Int -> Int -> Int
+(+) 2 2 = 5
+(+) a b = a P.+ b
 
 main :: IO ()
 main = (\_ -> putStrLn "hej,haj") * 3
