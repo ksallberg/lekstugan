3fae5506a1c1680fc7436aefbdb0d2d319f51eeb
Kristian Sallberg <kristian@purestyle.se>
2019-01-15 20:40:11 +0100

    servo and led display

---
+++ dispenser/README.md
@@ -0,0 +1,5 @@
+https://pdfserv.maximintegrated.com/en/ds/MAX7219-MAX7221.pdf
+
+https://www.sparkfun.com/datasheets/Components/General/COM-09622-MAX7219-MAX7221.pdf
+
+http://www.kingbright.com/attachments/file/psearch/000/00/00/CA56-21SURKWA(Ver.7A).pdf
--- dispenser/disp.c
+++ dispenser/disp.c
@@ -1,24 +1,56 @@
+#include "LedControl.h"
 #include <Servo.h>
 
-Servo myservo;
+// servo:
+// svarta ar jord
+// roda ar power (3 volt)
 
+// display:
+// pin 12 is connected to the DataIn (DIN)
+// pin 11 is connected to the CLK (left side CLK)
+// pin 10 is connected to LOAD
+// left vcc is connected to power (5 volt)
+
+LedControl lc=LedControl(12,11,10,1);
+
+Servo myservo;
 int pos = 0;
 int servopin = 2;
-int dir=90;
-
+unsigned long delaytime=250;
+int i=0;
 void setup() {
+
+  lc.shutdown(0,false);
+  lc.setIntensity(1,80);
+  lc.clearDisplay(0);
   myservo.attach(servopin);
 }
 
 void loop() {
-  for(pos = 0; pos <= dir; pos ++) {
-    myservo.write(pos);
-    delay(15);
-  }
-  delay(1500);
-  for(pos = dir; pos >= 0; pos --) {
-    myservo.write(pos);
-    delay(15);
-  }
-  delay(1500);
+
+    if(i == 8) {
+      i = 0;
+    }
+    lc.clearDisplay(0);
+
+    lc.setDigit(0,3,i,false);
+    lc.setDigit(0,2,i,false);
+    lc.setDigit(0,1,i,false);
+    lc.setDigit(0,0,i,false);
+
+    lc.setDigit(0,4,i+1,false);
+    lc.setDigit(0,5,i+1,false);
+    lc.setDigit(0,6,i+1,false);
+    lc.setDigit(0,7,i+1,false);
+
+    i++;
+    if(i==3) {
+       pos=0;
+       myservo.write(pos);
+    }
+    if(i==7) {
+       pos=0;
+       myservo.write(pos);
+    }
+    delay(delaytime);
 }
