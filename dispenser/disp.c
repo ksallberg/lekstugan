2056fd2bd3798a15d6303efd72e0a398034aedd8
Kristian Sallberg <kristian@purestyle.se>
2019-05-12 12:55:14 +0200

    new dispenser behavior

--- dispenser/disp.c
+++ dispenser/disp.c
@@ -15,6 +15,9 @@ LedControl lc=LedControl(12,11,10,1);
 
 Servo myservo;
 int high = 28800;
+//int high = 5;
+int low = 20;
+//int low = 4;
 int pos = 0;
 int servopin = 2;
 unsigned long delaytime=1000;
@@ -49,7 +52,6 @@ void loop() {
     //lc.setDigit(0,6,0,false);
     //lc.setDigit(0,5,0,false);
     lc.setDigit(0,4,dig5,false);
-
     lc.setDigit(0,3,dig4,false);
     lc.setDigit(0,2,dig3,false);
     lc.setDigit(0,1,dig2,false);
@@ -62,11 +64,17 @@ void loop() {
       } else {
         pos=100;
         myservo.attach(servopin);
+        myservo.write(160);
+        delay(200);
+        myservo.write(140);
+        delay(200);
+        myservo.write(120);
+        delay(200);
         myservo.write(pos);
-        delay(500);
+        delay(200);
         myservo.detach();
         state = 1;
-        counter = 20;
+        counter = low;
       }
     // down
     } else if(state ==1) {
