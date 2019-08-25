c72adb6f6337ad5c2bf632fd817a1ce6f793e13e
Kristian Sallberg <kristian@purestyle.se>
2019-08-25 20:41:10 +0200

    Revert "save progress with linked list"
    This reverts commit 5f607d79f9c4194da6302e104aa6707a1ec3dc37.

--- wordsnake/wordsnake.c
+++ wordsnake/wordsnake.c
@@ -19,6 +19,9 @@ const char *pokemons[] = {
   "wingull","yamask"};
 
 #define BUFSIZE 70
+#define CHEAT 24
+
+extern int cur_max_len = 0;
 
 struct Node {
   int id;
@@ -26,33 +29,45 @@ struct Node {
   list_t *edges_out;
 };
 
-void reset_max(list_t *max_chain, list_t *new_max) {
-  /* printf("old %lu new %lu\n", max_chain->len, new_max->len); */
-  list_node_t *x;
-  if(max_chain->len < new_max->len) {
+void remove_arr(int look_for, int chain[CHEAT]) {
+  int *rem = chain;
+  int *replace;
+  while(*rem != look_for) {
+    rem++;
+  }
+  // now standing at the thing to remove
+  replace = rem+1;
+  while(*replace != -1) {
+    *rem = *replace;
+    rem++;
+    replace++;
+  }
+  *rem = -1;
+}
 
-    while(max_chain->len > 0) {
-      list_lpop(max_chain);
+void reset_max(int max_chain[CHEAT], int new_max[CHEAT], int newlen) {
+  if(newlen > cur_max_len) {
+    cur_max_len = newlen;
+    for(int i = 0; i < CHEAT; i++) {
+      if(new_max[i] == -1) {
+        max_chain[i] = -1;
+        break;
+      }
+      max_chain[i] = new_max[i];
     }
-    /* list_destroy(max_chain); */
-    /* max_chain = list_new(); */
-
-    x = new_max->head;
-    while(x != NULL) {
-      list_rpush(max_chain, list_node_new(x->val));
-      x = x->next;
-    }
-    /* max_chain->head = new_max->head; */
   }
 }
 
-void dfs(int start_node, struct Node nodes[BUFSIZE], list_t *max_chain) {
+void dfs(int start_node, struct Node nodes[BUFSIZE], int max_chain[CHEAT]) {
   int stack[BUFSIZE];
   int node;
-  list_t *cur_chain;
+  int cur_chain[CHEAT];
   int stackpt = 0;
+  int cur_chain_last = 0;
 
-  cur_chain = list_new();
+  for(int i = 0; i < CHEAT; i ++) {
+    cur_chain[i] = -1;
+  }
 
   // stack
   stack[stackpt] = start_node;
@@ -61,20 +76,20 @@ void dfs(int start_node, struct Node nodes[BUFSIZE], list_t *max_chain) {
   while(stackpt > 0) {
     node = stack[stackpt-1];
     if(in_cur(node, cur_chain)==1) {
-      if(last_pos(cur_chain) == node) {
+      if(cur_chain[cur_chain_last-1] == node) {
         // pop stack
         stackpt--;
         // remove node from chain
-        list_rpop(cur_chain);
+        cur_chain[cur_chain_last] = -1;
+        cur_chain_last --;
       } else {
         // pop stack
         stackpt--;
       }
     } else {
-      list_node_t *new_node = list_node_new(node);
-      list_rpush(cur_chain, new_node);
-      list_t *temp = nodes[node].edges_out;
-      int edges_len = temp->len;
+      cur_chain[cur_chain_last] = node;
+      cur_chain_last ++;
+      int edges_len = nodes[node].edges_out->len;
       list_node_t *cur = nodes[node].edges_out->head;
       if(edges_len>0) {
         while(cur != NULL) {
@@ -83,43 +98,38 @@ void dfs(int start_node, struct Node nodes[BUFSIZE], list_t *max_chain) {
           stackpt++;
         }
       } else {
-        reset_max(max_chain, cur_chain);
+        reset_max(max_chain, cur_chain, cur_chain_last);
         //pop stack
         stackpt--;
         if (in_cur(node, cur_chain)==1) {
-          list_node_t *result = list_find(cur_chain, node);
-          list_remove(cur_chain, result);
+          remove_arr(node, cur_chain);
+          cur_chain_last --;
         }
       }
     }
   }
 }
 
-int last_pos(list_t *chain) {
-  list_node_t *result = list_at(chain, (chain->len)-1);
-  return result->val;
-}
-
-int in_cur(int look_for, list_t *chain) {
-  list_node_t *result = list_find(chain, look_for);
-  if(result == NULL)
-    return 0;
-  if(result->val == look_for) {
-    return 1;
-  } else {
-    return 0;
+int in_cur(int look_for, int chain[CHEAT]) {
+  for(int i=0; i < CHEAT; i++) {
+    if(chain[i] == look_for) {
+      return 1;
+    }
   }
+  return 0;
 }
 
 int main() {
-  list_t *max_chain;
+  int max_chain[CHEAT];
   struct Node nodes[BUFSIZE];
   char first_char;
   char last_char;
   int i, j;
   char comp;
 
-  max_chain = list_new();
+  for(int i = 0; i < CHEAT; i ++) {
+    max_chain[i] = -1;
+  }
 
   // build graph
   for(i = 0; i < BUFSIZE; i++) {
@@ -160,12 +170,11 @@ int main() {
     list_destroy(nodes[i].edges_out);
   }
 
-  list_node_t *it = max_chain->head;
-  if(it == NULL) printf("shit...\n");
-  while(it != NULL) {
-
-    printf("%s\n", pokemons[(int) it->val]);
-        it = it->next;
+  for(i=0; i < CHEAT; i++) {
+    if(max_chain[i] == -1) {
+      break;
+    }
+    printf("%s\n", pokemons[max_chain[i]]);
   }
 
   exit(0);
