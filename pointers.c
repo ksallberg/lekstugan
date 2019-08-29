39cd794b200689acabe529ca8116b4d411860294
Kristian Sallberg <kristian@purestyle.se>
2019-08-29 23:22:55 +0200

    double pointer test

--- pointers.c
+++ pointers.c
@@ -3,24 +3,19 @@ int main() {
   char *str2 = "var";
   char *str3 = "en";
   char *str4 = "gang";
-  char *str5 = ".";
 
-  char **sentence = {str1, str2, str3, str4, str5};
+  char **sentence[10];//{str1, str2, str3, str4, str5};
+  sentence[0] = str1;
+  sentence[1] = str2;
+  sentence[2] = str3;
+  sentence[3] = str4;
 
   char **x = sentence;
 
-  printf("%s", str1);
-  printf("%s", str2);
-  printf("%s", str3);
-
-  printf("\n");
-
-  printf(&sentence[0]);
-  printf(&sentence[1]);
-
-  printf("\n___");
-
-  printf(x);
+  for(int i = 0; i < 4; i ++) {
+    printf("%s ", *x);
+    x++;
+  }
 
   printf("\n");
 }
